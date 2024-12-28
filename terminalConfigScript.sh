968e45d5a843747466cdd36256af05cc1f0b3fcf
daniel <79683513+daniellichotti@users.noreply.github.com>
2024-12-28 19:15:23 -0300

    Update terminalConfigScript.sh

--- terminalConfigScript.sh
+++ terminalConfigScript.sh
@@ -4,7 +4,7 @@ set -e  # Interrompe o script em caso de erros
 
 # Instalar git e base-devel
 echo 'Instalando git e base-devel...'
-sudo pacman -S --needed --noconfirm git base-devel && echo "Git e base-devel instalados com sucesso!"
+sudo pacman -S --needed --noconfirm wget git base-devel && echo "Wget, Git e base-devel instalados com sucesso!"
 
 # Instalar Yay
 echo 'Instalando Yay...'
@@ -14,9 +14,23 @@ makepkg -si --noconfirm
 cd ..
 rm -rf yay && echo "Yay instalado com sucesso!"
 
+echo 'Instalando VSCode e Docker...'
+#Instalar pacotes com Yay
+yay -S --noconfirm visual-studio-code-bin docker
+echo 'Habilite a distro nas Resources do Docker Desktop...'
+
+#Instalar ferramentas dev
+yay -S --noconfirm vim neovim rust nodejs npm python python-pip
+
+#Astrovim
+git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim && rm -rf ~/.config/nvim/.git
+
+
 # Instalar pacotes úteis
 echo 'Instalando pacotes úteis...'
-sudo pacman -S --noconfirm zsh tmux zoxide exa figlet lolcat nnn bat fzf man-pages man-db fd yazi ffmpegthumbnailer ffmpeg p7zip jq poppler ripgrep imagemagick && echo "Pacotes instalados com sucesso!"
+sudo pacman -S --noconfirm zsh tmux zoxide exa figlet lolcat bat fzf man-pages man-db fd yazi ffmpegthumbnailer ffmpeg p7zip jq poppler ripgrep imagemagick 
+
+&& echo "Pacotes instalados com sucesso!"
 
 # Instalar OhMyZsh
 echo 'Instalando OhMyZsh...'
@@ -48,23 +62,5 @@ echo 'Baixando arquivos de configuração...'
 curl -o ~/.zshrc https://raw.githubusercontent.com/daniellichotti/linuxConfigs/main/zshrc
 curl -o ~/.config/starship.toml https://raw.githubusercontent.com/daniellichotti/linuxConfigs/main/starship
 curl -o ~/.tmux.conf https://raw.githubusercontent.com/daniellichotti/linuxConfigs/main/tmux.conf
-
-# Perguntar se o usuário deseja instalar o Node.js
-echo 'Você deseja instalar o Node.js?'
-echo '1 - Sim'
-echo '2 - Não'
-while true; do
-    read -p "Escolha uma opção [1/2]: " node_choice
-    if [ "$node_choice" == "1" ]; then
-        echo 'Instalando Node.js e npm...'
-        sudo pacman -S --noconfirm nodejs npm && echo "Node.js e npm instalados com sucesso!"
-        break
-    elif [ "$node_choice" == "2" ]; then
-        echo 'Node.js não será instalado.'
-        break
-    else
-        echo 'Opção inválida. Por favor, escolha 1 ou 2.'
-    fi
-done
 
 echo "Configuração concluída! Reinicie o terminal para aplicar as mudanças."
