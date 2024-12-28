006ae09fd0b42174efb8fe49d853b83e66dce2f2
daniel <79683513+daniellichotti@users.noreply.github.com>
2024-12-28 20:19:34 -0300

    Update terminalConfigScript.sh

--- terminalConfigScript.sh
+++ terminalConfigScript.sh
@@ -20,7 +20,7 @@ yay -S --noconfirm visual-studio-code-bin docker
 echo 'Habilite a distro nas Resources do Docker Desktop...'
 
 #Instalar ferramentas dev
-yay -S --noconfirm vim neovim rust nodejs npm python python-pip
+yay -S --noconfirm vim neovim rust nodejs npm python python-pip github-cli
 
 #Astrovim
 git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim && rm -rf ~/.config/nvim/.git
