38c385a6bb115ff42f1a437ffd28f8ea824c803b
daniel <79683513+daniellichotti@users.noreply.github.com>
2024-12-28 18:23:28 -0300

    Update terminalConfigScript.sh final

--- terminalConfigScript.sh
+++ terminalConfigScript.sh
@@ -1,45 +1,40 @@
 #!/bin/bash
 
+set -e  # Interrompe o script em caso de erros
+
 echo 'Instalando git e base-devel...'
-sudo pacman -S --needed git base-devel && echo "Git e base-devel instalados com sucesso!" || { echo "Erro ao instalar Git e base-devel."; exit 1; }
+sudo pacman -S --needed --noconfirm git base-devel && echo "Git e base-devel instalados com sucesso!"
 
 echo 'Instalando Yay...'
-git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd .. && rm -rf yay && echo "Yay instalado com sucesso!" 
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si --noconfirm
+cd ..
+rm -rf yay && echo "Yay instalado com sucesso!"
 
-echo 'Instalando zsh, tmux, zoxide, exa, figlet, lolcat, nnn, bat, fzf, man-pages, man-db, fd, yazi e outros pacotes...'
-sudo pacman -S --noconfirm zsh tmux zoxide exa figlet lolcat nnn bat fzf man-pages man-db fd yazi ffmpegthumbnailer ffmpeg p7zip jq poppler ripgrep imagemagick && echo "Pacotes instalados com sucesso!" || { echo "Erro ao instalar os pacotes."; exit 1; }
+echo 'Instalando pacotes úteis...'
+sudo pacman -S --noconfirm zsh tmux zoxide exa figlet lolcat nnn bat fzf man-pages man-db fd yazi ffmpegthumbnailer ffmpeg p7zip jq poppler ripgrep imagemagick && echo "Pacotes instalados com sucesso!"
 
 echo 'Instalando OhMyZsh...'
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --keep-zshrc && echo "OhMyZsh instalado com sucesso!" || { echo "Erro ao instalar OhMyZsh."; exit 1; }
-
-# Remover o export do SHELL com &&
-export SHELL=/bin/bash
+RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && echo "OhMyZsh instalado com sucesso!"
+sudo chsh -s /bin/zsh $(whoami) && echo "Shell padrão alterado para Zsh."
 
 echo 'Instalando Starship...'
-yes | curl -sS https://starship.rs/install.sh | sh && echo "Starship instalado com sucesso!" || { echo "Erro ao instalar Starship."; exit 1; }
+mkdir -p ~/.config
+curl -sS https://starship.rs/install.sh | sh && echo "Starship instalado com sucesso!"
 
-echo 'Instalando Zsh autosugestions...'
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions && echo "zsh-autosuggestions instalado com sucesso!" || { echo "Erro ao instalar zsh-autosuggestions."; exit 1; }
+echo 'Instalando Zsh autosuggestions...'
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions && echo "zsh-autosuggestions instalado com sucesso!"
 
 echo 'Instalando Zsh Syntax Highlighting...'
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting && echo "zsh-syntax-highlighting instalado com sucesso!" || { echo "Erro ao instalar zsh-syntax-highlighting."; exit 1; }
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting && echo "zsh-syntax-highlighting instalado com sucesso!"
 
 echo 'Instalando Atuin...'
-curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh && echo "Atuin instalado com sucesso!" || { echo "Erro ao instalar Atuin."; exit 1; }
+curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh && echo "Atuin instalado com sucesso!"
 
-echo 'Lembre-se de sincronizar!'
-echo 'Copiando meus arquivos .zshrc, starship.toml e tmux.conf...'
-
-# Usando && somente quando necessário para garantir que os downloads sejam feitos em sequência
-curl -o ~/.zshrc https://raw.githubusercontent.com/daniellichotti/linuxConfigs/main/zshrc && \
-curl -o ~/.config/starship.toml https://raw.githubusercontent.com/daniellichotti/linuxConfigs/main/starship && \
+echo 'Baixando arquivos de configuração...'
+curl -o ~/.zshrc https://raw.githubusercontent.com/daniellichotti/linuxConfigs/main/zshrc
+curl -o ~/.config/starship.toml https://raw.githubusercontent.com/daniellichotti/linuxConfigs/main/starship
 curl -o ~/.tmux.conf https://raw.githubusercontent.com/daniellichotti/linuxConfigs/main/tmux.conf
 
-
-# Alterando tema do ZSH
-#yay -S --noconfirm zsh-theme-powerlevel10k-git && echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc && chsh -s /usr/bin/zsh
-#Reinicie o terminal.
-# Para configurar:
-#p10k configure
-
-
+echo "Configuração concluída! Reinicie o terminal para aplicar as mudanças."
