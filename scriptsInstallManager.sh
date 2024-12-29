9e26fea357233d9463cde0519300a471cc6d996a
daniel <79683513+daniellichotti@users.noreply.github.com>
2024-12-28 21:44:37 -0300

    Update and rename testes.sh to scriptsInstallManager.sh

---
+++ scriptsInstallManager.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# curl -fsSL https://raw.githubusercontent.com/daniellichotti/linuxConfigs/refs/heads/main/scriptsinstallmanager.sh | zsh -s 1 2 3...
+
+# Verifica se argumentos foram passados
+if [ $# -gt 0 ]; then
+  echo "Argumentos passados: $@"
+  # Verifica o valor do primeiro argumento
+  if [ "$1" == "1" ]; then
+    echo "Executando opção 1: "
+    curl -fsSL https://raw.githubusercontent.com/daniellichotti/linuxConfigs/refs/heads/main/terminalConfigScript.sh | bash
+    # Coloque o código para executar a ação para 'argumento1' aqui
+  elif [ "$1" == "2" ]; then
+    echo "Executando opção 2: "
+    # Coloque o código para executar a ação para 'argumento2' aqui
+  elif [ "$1" == "3" ]; then
+    echo "Executando opção 3: "
+    # Coloque o código para executar a ação para 'argumento2' aqui
+  else
+    echo "Opção desconhecida: $1"
+  fi
+else
+  echo "Adicione ao fim: "
+  echo "1 para instalar aplicações necessárias e customizar o terminal"
+  echo "2 para dev"
+  echo "3 para outra coisa"
+  # Código para configuração padrão
+fi
