8b04ae7d8ead83b0c8ddeb4da8ea3cfa516c064f
daniel <79683513+daniellichotti@users.noreply.github.com>
2024-12-28 21:34:44 -0300

    Update testes.sh

--- testes.sh
+++ testes.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 # curl -fsSL https://raw.githubusercontent.com/daniellichotti/linuxConfigs/refs/heads/main/testes.sh | zsh -s argumento1 argumento2
-# Verifica o número de argumentos
+
+# Verifica se argumentos foram passados
 if [ $# -gt 0 ]; then
   echo "Argumentos passados: $@"
   # Verifica o valor do primeiro argumento
-  if [ "$1" == "opcao1" ]; then
-    echo "Executando ação para opção 1"
-    # Coloque o código para executar a ação para 'opcao1' aqui
-  elif [ "$1" == "opcao2" ]; then
-    echo "Executando ação para opção 2"
-    # Coloque o código para executar a ação para 'opcao2' aqui
+  if [ "$1" == "argumento1" ]; then
+    echo "Executando ação para argumento1"
+    # Coloque o código para executar a ação para 'argumento1' aqui
+  elif [ "$1" == "argumento2" ]; then
+    echo "Executando ação para argumento2"
+    # Coloque o código para executar a ação para 'argumento2' aqui
   else
     echo "Opção desconhecida: $1"
   fi
