a02bbfbe6536455418330567d54aa52fd7528f21
daniel <79683513+daniellichotti@users.noreply.github.com>
2024-12-28 23:05:40 -0300

    Update createProject.sh

--- createProject.sh
+++ createProject.sh
@@ -20,41 +20,6 @@ case "$opcao" in
         echo "Criando Projeto React Native com Expo..."
         npx create-expo-app --template
         ;;
-    3)
-        echo -n "Digite o nome do projeto: "
-        read nomeProjeto
-        mkdir "$nomeProjeto"
-        cd "$nomeProjeto"
-        python -m venv venv
-        source venv/bin/activate
-        pip install flask
-        # Criar o arquivo app.py e adicionar o conteúdo
-        cat <<EOL > app.py
-        from flask import Flask
-        
-        app = Flask(__name__)
-        
-        @app.route('/')
-        def home():
-            return 'Olá, Flask!'
-        
-        if __name__ == '__main__':
-            app.run(debug=True)
-        EOL
-        echo "Para iniciar o server: python app.py"
-        echo "Para sair do venv: deactivate"
-        ;;
-    4)
-        echo -n "Digite o nome do projeto: "
-        read nomeProjeto
-        mkdir "$nomeProjeto"
-        cd "$nomeProjeto"
-        python -m venv venv
-        source venv/bin/activate
-        pip install django
-        django-admin startproject "$nomeProjeto" .
-        echo "Para iniciar o server: python manage.py runserver"
-        ;;
     0)
         echo "Saindo..."
         ;;
