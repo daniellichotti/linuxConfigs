e8dc54b1b70c2314c7c1721bdf654fe6d897754f
daniel <79683513+daniellichotti@users.noreply.github.com>
2024-12-28 22:04:47 -0300

    Update scriptsInstallManager.sh

--- scriptsInstallManager.sh
+++ scriptsInstallManager.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-# curl -fsSL https://raw.githubusercontent.com/daniellichotti/linuxConfigs/refs/heads/main/scriptsInstallManager.sh | bash -s 1 2 3...
+# curl -fsSL https://raw.githubusercontent.com/daniellichotti/linuxConfigs/refs/heads/main/scriptsInstallManager.sh | zsh -s 1 2 3...
 
 # Verifica se argumentos foram passados
 if [ $# -gt 0 ]; then
   echo "Argumentos passados: $@"
+  
   # Verifica o valor do primeiro argumento
   if [ "$1" == "1" ]; then
     echo "Executando opção 1: "
-    curl -fsSL https://raw.githubusercontent.com/daniellichotti/linuxConfigs/refs/heads/main/terminalConfigScript.sh | bash
+    curl -fsSL https://raw.githubusercontent.com/daniellichotti/linuxConfigs/refs/heads/main/terminalConfigScript.sh | zsh
     # Coloque o código para executar a ação para 'argumento1' aqui
   elif [ "$1" == "2" ]; then
     echo "Executando opção 2: "
     wget  https://raw.githubusercontent.com/daniellichotti/linuxConfigs/refs/heads/main/createProject.sh && zsh ./createProject.sh && rm createProject.sh
   elif [ "$1" == "3" ]; then
     echo "Executando opção 3: "
-    # Coloque o código para executar a ação para 'argumento2' aqui
+    # Coloque o código para executar a ação para 'argumento3' aqui
   else
     echo "Opção desconhecida: $1"
   fi
